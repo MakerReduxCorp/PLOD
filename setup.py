789828b8a1ac2495bc47832131121291eee47d2c
John Dupuy <johnd@makerredux.com>
2014-07-08 16:46:54 -0500

    created distribution 0.1.4 for pypi then posted it.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='PLOD',
-    version='0.1.3c',
+    version='0.1.4',
     packages=['PLOD',],
-    license='BSD',
-    long_description=open('README.txt').read()
+    license='MIT',
+    long_description=open('README.rst').read()
 )
